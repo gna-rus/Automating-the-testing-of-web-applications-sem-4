f2444398f078b342ccbe5dc49fae77cff4daceb3
gnarus-tm <71601039+gna-rus@users.noreply.github.com>
2024-02-20 09:48:59 +0300

    Update Task1_sem4.py

--- Task1_sem4.py
+++ Task1_sem4.py
@@ -11,30 +11,36 @@ from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.chrome import ChromeDriverManager
 import json
 import requests
+from logger import log_all
+import logging
 
+FORMAT = '{levelname:<8} - {asctime}. В модуле "{name}" в строке {lineno:03d} функция "{funcName}()" в {created} секунд записала сообщение: {msg}'
+logging.basicConfig(format=FORMAT, style='{',filename='project.log.', filemode='w', encoding='utf-8', level=logging.INFO) # сохранаяю результаты лоиггирования в отдельный файл
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('Логгирование:')
+logger.info('___')
 
-# библиотеки для скачивания драйверов браузеров
 
 with open("./locators.yaml") as f:
     locators = yaml.safe_load(f)
 
-
 class Site:
     # проверка на то какой браузер используется в тесте
     def __init__(self, browser, address):
+        logger.info('Инициализация теста')
         self.browser = browser
         self.address = address
 
         if self.browser == 'chrome':
+            logger.info('Browser: Chrome')
             self.driver = webdriver.Chrome()
         elif self.browser == 'firefox':
+            logger.info('Browser: Firefox')
             self.driver = webdriver.Chrome()
 
-        self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(testdata['sleep_time'])
         self.driver.maximize_window()
         self.driver.get(address)
-
 
 
     def registration_on_the_website(self):
@@ -84,6 +90,46 @@ class Site:
     def close(self):
         self.driver.close()
 
+class Side_API:
+    def decorator_loggin(self, func):
+        @functools.wraps(func)
+        def wrapper():
+            response_post = requests.post(testdata['url_login'],
+                                          data={'username': testdata['username'], 'password': testdata['passwd']})
+            return func(response_post.json()['token'])
+        return wrapper
+
+    @decorator_loggin
+    def generate_post(self, token):
+        # функция создающая пост
+        in_post = {"title": "test_title", "description": "test_description",
+                   "content": "test_content"}  # передаваемые данные
+
+        # передача поста (передается токкен регистрации и данные)
+        response_post = requests.post(testdata['url_post'], headers={"X-Auth-Token": token}, data=in_post)
+        answer_code = response_post.status_code
+        return response_post.json(), answer_code
+
+    @decorator_loggin
+    def find_post(self, token):
+        resource = requests.get(testdata['url_profil'], headers={"X-Auth-Token": token})
+        return resource.json()
+
+    @decorator_loggin
+    def get_post(self, token):
+        # возрвращае состояние поста
+        resource = requests.get(testdata['url_post'], headers={"X-Auth-Token": token}, params={"owner": "notMe"})
+        return resource.json()
+
+    def find_id(self, find_id):
+        # функция ищет переданный в нее id в посте
+        result = self.get_post()
+        flag = False
+        for item in result['data']:
+            print(item)
+            if find_id == item['id']:
+                flag = True
+        return flag
 
 # файл конфигурации теста
 with open("./testdata.yaml") as f:
@@ -95,68 +141,29 @@ with open("./testdata.yaml") as f:
 
 
 
-def decorator_loggin(func):
-    @functools.wraps(func)
-    def wrapper():
-        response_post = requests.post(testdata['url_login'], data={'username': testdata['username'], 'password': testdata['passwd']})
-        return func(response_post.json()['token'])
-    return wrapper
+test_API = Side_API()
 
-@decorator_loggin
-def generate_post(token):
-    # функция создающая пост
-    in_post = {"title": "test_title", "description": "test_description", "content": "test_content"} # передаваемые данные
-
-    # передачаа поста (передается токкен регистрации и данные)
-    response_post = requests.post(testdata['url_post'], headers={"X-Auth-Token": token}, data=in_post)
-    answer_code = response_post.status_code
-    return response_post.json(), answer_code
-
-@decorator_loggin
-def find_post(token):
-    resource = requests.get(testdata['url_profil'], headers={"X-Auth-Token": token})
-    return resource.json()
-
-@decorator_loggin
-def get_post(token):
-    # возрвращае состояние поста
-    resource = requests.get(testdata['url_post'], headers={"X-Auth-Token": token}, params={"owner": "notMe"})
-    return resource.json()
-
-
-def find_id(find_id):
-    # функция ищет переданный в нее id в посте
-    result = get_post()
-    flag = False
-    for item in result['data']:
-        print(item)
-        if find_id == item['id']:
-            flag = True
-    return flag
-
-
-result_test_of_generate = generate_post()
 
 def test_find_id():
+    site_bed = Site(testdata["browser"], testdata['addres'])
     #Тест на поиск id в ответе
-    assert find_id(98745) == True, 'Test of find id - False'
-
+    assert test_API.find_id(98745) == True, 'Test of find id - False'
 
 def test_generate_post_code():
     #Тест на определение кода ответа
-    assert result_test_of_generate[1] == 200, 'Test code of generate post - False'
+    assert test_API.generate_post[1] == 200, 'Test code of generate post - False'
 
 def test_generate_post_title():
     # Тест на поиск Title в созданном посте
-    assert result_test_of_generate[0]['title'] == 'test_title', 'Test: Title - False'
+    assert test_API.generate_post[0]['title'] == 'test_title', 'Test: Title - False'
 
 def test_generate_post_description():
     # Тест на поиск description в созданном посте
-    assert result_test_of_generate[0]['description'] == 'test_description', 'Test: Description - False'
+    assert test_API.generate_post[0]['description'] == 'test_description', 'Test: Description - False'
 
 def test_generate_post_content():
     # Тест на поиск content в созданном посте
-    assert result_test_of_generate[0]['content'] == 'test_content', 'Test: Content - False'
+    assert test_API.generate_post[0]['content'] == 'test_content', 'Test: Content - False'
 
 # def test_step1():
 #     # Тест при не правильном вводе данных пользователя
@@ -302,3 +309,6 @@ def test_generate_post_content():
 # text = msg.as_string()
 # server.sendmail(fromaddr, to_address, text)
 # server.quit()
+
+
+log_all()
